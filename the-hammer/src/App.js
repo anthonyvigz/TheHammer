02222d7577c7508b7e70d2bfce87b231e24206e3
anthonyvigz <anthonyvigz@gmail.com>
2020-07-21 18:39:37 -0400

    successfully got watchlist of day

--- the-hammer/src/App.js
+++ the-hammer/src/App.js
@@ -1,24 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./styling.scss";
 
 function App() {
+  const [list, setList] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`https://the-pavilion-hammer.herokuapp.com/data/get`)
+      .then((res) => {
+        const length = res.data.data.length;
+        const newlist = res.data.data[length - 1]["watchlist"].split(" ");
+        const finalist = [];
+        newlist.map((item) => {
+          const newitem = item.slice(0, -1);
+          finalist.push(newitem);
+        });
+        setList(finalist);
+      });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {list.map((item) => {
+        return <h1>{item}</h1>;
+      })}
     </div>
   );
 }
