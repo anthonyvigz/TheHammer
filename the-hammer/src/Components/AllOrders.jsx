d90c9e0e0cb0758a9462518a1874568c3b7a7eda
Anthony Vigliotta <anthonyvigz@gmail.com>
2020-12-13 12:01:46 -0500

    set up dark theme

--- the-hammer/src/Components/AllOrders.jsx
+++ the-hammer/src/Components/AllOrders.jsx
@@ -1,55 +1,54 @@
-import React, { useEffect, useState } from 'react';
-import Link from '@material-ui/core/Link';
-import { makeStyles } from '@material-ui/core/styles';
-import axios from 'axios';
-import Paper from '@material-ui/core/Paper';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TablePagination from '@material-ui/core/TablePagination';
-import TableRow from '@material-ui/core/TableRow';
+import React, { useEffect, useState } from "react";
+import Link from "@material-ui/core/Link";
+import { makeStyles } from "@material-ui/core/styles";
+import axios from "axios";
+import Paper from "@material-ui/core/Paper";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TablePagination from "@material-ui/core/TablePagination";
+import TableRow from "@material-ui/core/TableRow";
 
 let today = new Date();
-const dd = String(today.getDate()).padStart(2, '0');
-const mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+const dd = String(today.getDate()).padStart(2, "0");
+const mm = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
 const yyyy = today.getFullYear();
 
-today = mm + '/' + dd + '/' + yyyy;
+today = mm + "/" + dd + "/" + yyyy;
 
 const columns = [
-  { id: 'date', label: 'Date', minWidth: 120 },
-  { id: 'time', label: 'Time', minWidth: 120 },
-  { id: 'symbol', label: 'Symbol', minWidth: 100 },
-  { id: 'smaEntryDelta', label: 'SMA Delta', minWidth: 120 },
+  { id: "date", label: "Date", minWidth: 120 },
+  { id: "time", label: "Time", minWidth: 120 },
+  { id: "symbol", label: "Symbol", minWidth: 100 },
+  { id: "smaEntryDelta", label: "SMA Delta", minWidth: 120 },
   {
-    id: 'entryPrice',
-    label: 'Entry Price',
+    id: "entryPrice",
+    label: "Entry Price",
     minWidth: 90,
-    align: 'right',
-    format: (value) => value.toLocaleString('en-US'),
+    align: "right",
+    format: (value) => value.toLocaleString("en-US"),
   },
   {
-    id: 'orderType',
-    label: 'Order Type',
+    id: "orderType",
+    label: "Order Type",
     minWidth: 120,
-    align: 'right',
-    format: (value) => value.toLocaleString('en-US'),
+    align: "right",
+    format: (value) => value.toLocaleString("en-US"),
   },
   {
-    id: 'shares',
-    label: 'Shares',
+    id: "shares",
+    label: "Shares",
     minWidth: 100,
-    align: 'right',
+    align: "right",
     format: (value) => value.toFixed(2),
   },
 ];
 
-
 const useStyles = makeStyles({
   root: {
-    width: '100%',
+    width: "100%",
   },
   container: {
     maxHeight: 440,
@@ -70,70 +69,80 @@ export default function AllOrders() {
     setPage(0);
   };
 
-  const [ todayData, setTodayData ] = useState([]);
+  const [todayData, setTodayData] = useState([]);
   const rows = todayData;
 
   useEffect(() => {
-      axios
-          .get('https://api.jsonbin.io/b/5fd1536681ec296ae71bf145/latest', {
-              headers: {
-                  'secret-key': '$2b$10$W9pzC938HHmXjvu/mRyO0.EKQPhhHssueyYCpi7wQ2IqTXsXNQ4Ga'
-              }
-          })
-          .then((res) => {
-              setTodayData(res.data["Orders"]);
-          })
-          .catch((err) => {
-              console.log(err);
-          });
+    axios
+      .get("https://api.jsonbin.io/b/5fd1536681ec296ae71bf145/latest", {
+        headers: {
+          "secret-key":
+            "$2b$10$W9pzC938HHmXjvu/mRyO0.EKQPhhHssueyYCpi7wQ2IqTXsXNQ4Ga",
+        },
+      })
+      .then((res) => {
+        setTodayData(res.data["Orders"]);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }, []);
 
   return (
-      <div id="ordersTable">
-    <Paper className={classes.root}>
-      <TableContainer className={classes.container}>
-        <Table stickyHeader aria-label="sticky table">
-          <TableHead>
-            <TableRow>
-              {columns.map((column) => (
-                <TableCell
-                  key={column.id}
-                  align={column.align}
-                  style={{ minWidth: column.minWidth }}
-                >
-                  {column.label}
-                </TableCell>
-              ))}
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => {
-              return (
-                <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
-                  {columns.map((column) => {
-                    const value = row[column.id];
-                    return (
-                      <TableCell key={column.id} align={column.align}>
-                        {column.format && typeof value === 'number' ? column.format(value) : value}
-                      </TableCell>
-                    );
-                  })}
-                </TableRow>
-              );
-            })}
-          </TableBody>
-        </Table>
-      </TableContainer>
-      <TablePagination
-        rowsPerPageOptions={[10, 25, 100]}
-        component="div"
-        count={rows.length}
-        rowsPerPage={rowsPerPage}
-        page={page}
-        onChangePage={handleChangePage}
-        onChangeRowsPerPage={handleChangeRowsPerPage}
-      />
-    </Paper>
+    <div id="ordersTable">
+      <Paper className={classes.root}>
+        <TableContainer className={classes.container}>
+          <Table stickyHeader aria-label="sticky table">
+            <TableHead>
+              <TableRow>
+                {columns.map((column) => (
+                  <TableCell
+                    key={column.id}
+                    align={column.align}
+                    style={{ minWidth: column.minWidth }}
+                  >
+                    {column.label}
+                  </TableCell>
+                ))}
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows
+                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                .map((row) => {
+                  return (
+                    <TableRow
+                      hover
+                      role="checkbox"
+                      tabIndex={-1}
+                      key={row.code}
+                    >
+                      {columns.map((column) => {
+                        const value = row[column.id];
+                        return (
+                          <TableCell key={column.id} align={column.align}>
+                            {column.format && typeof value === "number"
+                              ? column.format(value)
+                              : value}
+                          </TableCell>
+                        );
+                      })}
+                    </TableRow>
+                  );
+                })}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <TablePagination
+          rowsPerPageOptions={[10, 25, 100]}
+          component="div"
+          count={rows.length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onChangePage={handleChangePage}
+          onChangeRowsPerPage={handleChangeRowsPerPage}
+        />
+      </Paper>
     </div>
   );
 }
--- the-hammer/src/index.js
+++ the-hammer/src/index.js
@@ -3,10 +3,26 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import amber from '@material-ui/core/colors/amber';
+
+const theme = createMuiTheme({
+  palette: {
+    type: "dark",
+    primary: amber,
+    secondary: {
+      main: '#ff3d00',
+    },
+  },
+});
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </BrowserRouter>,
   document.getElementById("root")
 );
